a91f8b028c63bf928fbbc7152c9f3fe39b19ac41
SuganthiM25 <suganthimanickam222@gmail.com>
2024-12-08 08:55:57 +0530

    Add files via upload

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		int caty = 10;
+		String cato = "wer";
+		System.out.print("Welcome to TestLeaf \n"+ caty + cato);
+	}
+
+}
